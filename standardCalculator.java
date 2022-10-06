0011bb2aa39aafc64cc408126598e85950bcca57
vidhya-a-mariappan <112065733+vidhya-a-mariappan@users.noreply.github.com>
2022-10-06 17:22:57 +0530

    Update standardCalculator.java

--- standardCalculator.java
+++ standardCalculator.java
@@ -3,46 +3,46 @@ package zsm.concole.calculator;
 import java.util.Scanner;
 import java.util.Stack;
 
-class standardCalculator {
-    void mainstandardcalculator() {
+class StandardCalculator {
+    void mainStandardCalculator() {
         double result=0;
         System.out.println("Enter the expression");
         Scanner sc = new Scanner(System.in);
-        String StrExp = sc.nextLine();
-        char[] tokens = StrExp.toCharArray();
+        String stringExpression = sc.nextLine();
+        char[] element = stringExpression.toCharArray();
         Stack<Double> values = new Stack<Double>();
         Stack<Character> operators= new Stack<Character>();
-        for (int i = 0; i < tokens.length; i++) {
-            if (tokens[i] == ' ')
+        for (int index = 0; index < element.length; index++) {
+            if (element[index] == ' ')
                 continue;
-            if (tokens[i] >= '0' && tokens[i] <= '9') {
+            if (element[index] >= '0' && element[index] <= '9') {
                 StringBuffer sbuf = new StringBuffer();
                 // checking for number more than one digit
-                while (i < tokens.length && tokens[i] >= '0' && tokens[i] <= '9')
-                    sbuf.append(tokens[i++]);
+                while (index < element.length && element[index] >= '0' && element[index] <= '9')
+                    sbuf.append(element[index++]);
                 values.push((double) Integer.parseInt(sbuf.toString()));
-                i--;//offsetting to correct index
+                index--;//offsetting to correct index
             }
-            else if (tokens[i] == '(')
-                operators.push(tokens[i]);
-            else if (tokens[i] == ')') {
+            else if (element[index] == '(')
+                operators.push(element[index]);
+            else if (element[index] == ')') {
                 while (operators.peek() != '(')
-                    values.push(evaluateExp(operators.pop(),values.pop(),values.pop()));
+                    values.push(evaluateExpression(operators.pop(),values.pop(),values.pop()));
                 operators.pop();
             }
-            else if (tokens[i] == '+' || tokens[i] == '-' || tokens[i] == '*' || tokens[i] == '/') {
-                while (!operators.empty() && checkprecedence(tokens[i],operators.peek()))
-                    values.push(evaluateExp(operators.pop(),values.pop(),values.pop()));
-                operators.push(tokens[i]);
+            else if (element[index] == '+' || element[index] == '-' || element[index] == '*' || element[index] == '/') {
+                while (!operators.empty() && checkPrecedence(element[index],operators.peek()))
+                    values.push(evaluateExpression(operators.pop(),values.pop(),values.pop()));
+                operators.push(element[index]);
             }
         }
         while (!operators.empty())
-            values.push(evaluateExp(operators.pop(),values.pop(),values.pop()));
+            values.push(evaluateExpression(operators.pop(),values.pop(),values.pop()));
         result=values.pop();
         System.out.println(result);
     }
 
-    public static boolean checkprecedence(char op1, char op2) {
+    public static boolean checkPrecedence(char op1, char op2) {
         if (op2 == '(' || op2 == ')')
             return false;
         if ((op1 == '*' || op1 == '/') && (op2 == '+' || op2 == '-'))
@@ -50,7 +50,7 @@ class standardCalculator {
         else
             return true;
     }
-    public static double evaluateExp(char op,double b, double a) {
+    public static double evaluateExpression(char op, double b, double a) {
         switch (op) {
             case '+':
                 return a + b;
