3b1c15baab1d5225e8ee18f9a3db3c90fb24b2ab
vidhya-a-mariappan <112065733+vidhya-a-mariappan@users.noreply.github.com>
2022-10-06 17:24:05 +0530

    Update sceintificCalculator.java

--- sceintificCalculator.java
+++ sceintificCalculator.java
@@ -2,13 +2,13 @@ package zsm.concole.calculator;
 
 import java.util.Scanner;
 
-class sceintificCalculator {
+class SceintificCalculator {
 
-    void mainscientificcalculator() {
+    void mainScientificCalculator() {
         int option = 0;
         double numOne = 0, numTwo = 0, result = 0, rad = 0;
         String Str = "", input = "";
-        Scanner sc = new Scanner(System.in);
+        Scanner scan = new Scanner(System.in);
         do {
             System.out.println("Choose any scientific manipulation");
             System.out.printf("%-25s %-25s %-25s %-25s %-25s\n"," 1. %"," 2.1/x"," 3.sqrt of x"," 4.cube root of x"," 5.n!");
@@ -16,151 +16,151 @@ class sceintificCalculator {
             System.out.printf("%-25s %-25s %-25s %-25s %-25s\n","11.Decimal to Binary","12.Binary to Decimal","13.Decimal to Octal","14.Octal to Decimal","15.Binary to Hexadecimal");
             System.out.printf("%-25s %-25s %-25s %-25s %-25s\n","16.Hexadecimal to Binary","17.sin","18.cos","19.tan","20.exponential");
             System.out.printf("%-25s %-25s %-25s %-25s %-25s\n","21.log","22.log10","23.Radians to Degree","24.Degree to Radian","25.Quit");
-            option = sc.nextInt();
+            option = scan.nextInt();
             switch (option) {
                 case 1:
                     System.out.println("Enter the 2 numbers to find percentage,say 25% of 100,here x=25 and y=100");
-                    numOne = sc.nextInt();
-                    numTwo = sc.nextInt();
+                    numOne = scan.nextInt();
+                    numTwo = scan.nextInt();
                     result = numTwo * (numOne / 100);
                     System.out.println(result);
                     break;
                 case 2:
                     System.out.println("x?");
-                    numOne = sc.nextInt();
+                    numOne = scan.nextInt();
                     result = 1 / numOne;
                     System.out.println(result);
                     break;
                 case 3:
                     System.out.println("x?");
-                    numOne = sc.nextInt();
+                    numOne = scan.nextInt();
                     result = Math.sqrt(numOne);
                     System.out.println(result);
                     break;
                 case 4:
                     System.out.println("x?");
-                    numOne = sc.nextInt();
+                    numOne = scan.nextInt();
                     result = Math.cbrt(numOne);
                     System.out.println(result);
                     break;
                 case 5:
                     System.out.println("n?");
-                    numOne = sc.nextInt();
+                    numOne = scan.nextInt();
                     result = factorial(numOne);
                     System.out.println(result);
                     break;
                 case 6:
                     System.out.println("Enter x and y");
-                    numOne = sc.nextInt();
-                    numTwo = sc.nextInt();
+                    numOne = scan.nextInt();
+                    numTwo = scan.nextInt();
                     result = Math.pow(numOne, numTwo);
                     System.out.println(result);
                     break;
                 case 7:
                     System.out.println("x?");
-                    numOne = sc.nextInt();
+                    numOne = scan.nextInt();
                     result = Math.pow(2, numOne);
                     System.out.println(result);
                     break;
                 case 8:
                     System.out.println("x?");
-                    numOne = sc.nextInt();
+                    numOne = scan.nextInt();
                     result = Math.pow(10, numOne);
                     System.out.println(result);
                     break;
                 case 9:
                     System.out.println("x?");
-                    numOne = sc.nextInt();
+                    numOne = scan.nextInt();
                     result = Math.pow(numOne, 2);
                     System.out.println(result);
                     break;
                 case 10:
                     System.out.println("x?");
-                    numOne = sc.nextInt();
+                    numOne = scan.nextInt();
                     result = Math.pow(numOne, 3);
                     System.out.println(result);
                     break;
                 case 11:
                     System.out.println("Enter the number");
-                    int dec = sc.nextInt();
+                    int dec = scan.nextInt();
                     Str = Integer.toBinaryString(dec);
                     System.out.println(Str);
                     break;
                 case 12:
                     System.out.println("Enter the binary number");
-                    input = sc.next();
+                    input = scan.next();
                     result = Integer.parseInt(input, 2);
                     System.out.println(result);
                     break;
                 case 13:
                     System.out.println("Enter the number");
-                    dec = sc.nextInt();
+                    dec = scan.nextInt();
                     Str = Integer.toOctalString(dec);
                     System.out.println(Str);
                     break;
                 case 14:
                     System.out.println("Enter the Octal number");
-                    input = sc.next();
+                    input = scan.next();
                     result = Integer.parseInt(input, 8);
                     System.out.println(result);
                     break;
                 case 15:
                     System.out.println("Enter the number");
-                    dec = sc.nextInt();
+                    dec = scan.nextInt();
                     Str = Integer.toHexString(dec);
                     System.out.println(Str);
                     break;
                 case 16:
                     System.out.println("Enter the Hexadecimal number");
-                    input = sc.next();
+                    input = scan.next();
                     result = Integer.parseInt(input, 16);
                     System.out.println(result);
                     break;
                 case 17:
                     System.out.println("Enter theta");
-                    numOne = sc.nextInt();
+                    numOne = scan.nextInt();
                     result = Math.sin(numOne);
                     System.out.println(result);
                     break;
                 case 18:
                     System.out.println("Enter theta");
-                    numOne = sc.nextInt();
+                    numOne = scan.nextInt();
                     result = Math.cos(numOne);
                     System.out.println(result);
                     break;
                 case 19:
                     System.out.println("Enter theta");
-                    numOne = sc.nextInt();
+                    numOne = scan.nextInt();
                     result = Math.tan(numOne);
                     System.out.println(result);
                     break;
                 case 20:
                     System.out.println("Enter the number");
-                    numOne = sc.nextInt();
+                    numOne = scan.nextInt();
                     result = Math.exp(numOne);
                     System.out.println(result);
                     break;
                 case 21:
                     System.out.println("Enter the number");
-                    numOne = sc.nextInt();
+                    numOne = scan.nextInt();
                     result = Math.log(numOne);
                     System.out.println(result);
                     break;
                 case 22:
                     System.out.println("Enter the number");
-                    numOne = sc.nextInt();
+                    numOne = scan.nextInt();
                     result = Math.log10(numOne);
                     System.out.println(result);
                     break;
                 case 23:
                     System.out.println("Enter Radian Angle");
-                    rad = sc.nextDouble();
+                    rad = scan.nextDouble();
                     result = Math.toDegrees(rad);
                     System.out.println(result);
                     break;
                 case 24:
                     System.out.println("Enter the number");
-                    numOne = sc.nextInt();
+                    numOne = scan.nextInt();
                     result = Math.toRadians(numOne);
                     System.out.println(result);
                     break;
